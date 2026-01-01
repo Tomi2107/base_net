3bc07a5128413e9f2f24c4c0eded175e08b3994c
Tomi07 <tom.palmeyro@gmail.com>
2026-01-01 20:35:00 -0300

    Fix settings

--- core/settings.py
+++ core/settings.py
@@ -124,8 +124,8 @@ TEMPLATES[0]['OPTIONS']['context_processors'] += [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-
 DATABASES = { 'default': { 'ENGINE': 'django.db.backends.mysql', 'NAME': 'mascotas25$default', 'USER': 'mascotas25', 'PASSWORD': '_$SgS$X6w2uPkPz', 'HOST': 'mascotas25.mysql.pythonanywhere-services.com', 'PORT': '3306', 'OPTIONS': { 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'", }, } }
+
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 
